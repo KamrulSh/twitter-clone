c4047254ac0dc5b03b13b73807dbc20dc8bf5ebd
Kamrul Islam Shahin <44506464+shahin-cuet@users.noreply.github.com>
2021-02-03 17:37:37 +0600

    design feed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
+import Feed from "./Feed";
 import Sidebar from "./Sidebar";
+import Widgets from "./Widgets";
 
 function App() {
     return (
         <div className="app">
-            <h1>This is twitter</h1>
             {/* sidebar */}
             <Sidebar />
             {/* feed */}
-
+            <Feed />
             {/* widgets */}
+            <Widgets />
         </div>
     );
 }
---
+++ src/Feed.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Feed.css";
+function Feed() {
+    return (
+        <div className="feed">
+            <h2 className="feed__header">Home</h2>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+            <p>Ssssssss</p>
+        </div>
+    );
+}
+
+export default Feed;
