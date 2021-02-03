cc87c497a40c1c160d398b8af58065aed5c532bb
Kamrul Islam Shahin <44506464+shahin-cuet@users.noreply.github.com>
2021-02-03 23:55:49 +0600

    add widgets

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "./Widgets.css";
+import SearchIcon from "@material-ui/icons/Search";
+import { TwitterTweetEmbed, TwitterTimelineEmbed } from "react-twitter-embed";
 
 function Widgets() {
     return (
         <div className="widgets">
-            <h1>widgets</h1>
+            <div className="widgets__input">
+                <SearchIcon className="widgets__searchIcon" />
+                <input placeholder="Search Twitter" type="text" />
+            </div>
+            <div className="widgets__widgetContainer">
+                <h2>What's trending</h2>
+                <TwitterTweetEmbed tweetId="1356998095733579776" />
+                <TwitterTimelineEmbed
+                    sourceType="profile"
+                    screenName="UWBadgers"
+                    options={{ height: 600 }}
+                />
+            </div>
         </div>
     );
 }
