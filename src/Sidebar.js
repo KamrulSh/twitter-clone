c6b9a7af458211e12b376c3e09a04333cf273da9
Kamrul Islam Shahin <44506464+shahin-cuet@users.noreply.github.com>
2021-02-03 16:29:24 +0600

    build left sidebar

---
+++ src/Sidebar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Sidebar.css";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import SidebarOption from "./SidebarOption";
+import HomeIcon from "@material-ui/icons/Home";
+import ExploreIcon from "@material-ui/icons/Explore";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import BookmarkIcon from "@material-ui/icons/Bookmark";
+import ListAltIcon from "@material-ui/icons/ListAlt";
+import PersonIcon from "@material-ui/icons/Person";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import { Button } from "@material-ui/core";
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <TwitterIcon />
+            <SidebarOption active Icon={HomeIcon} text="Home" />
+            <SidebarOption Icon={ExploreIcon} text="Explore" />
+            <SidebarOption Icon={NotificationsIcon} text="Notifications" />
+            <SidebarOption Icon={MailOutlineIcon} text="Messages" />
+            <SidebarOption Icon={BookmarkIcon} text="Bookmarks" />
+            <SidebarOption Icon={ListAltIcon} text="Lists" />
+            <SidebarOption Icon={PersonIcon} text="Profile" />
+            <SidebarOption Icon={MoreHorizIcon} text="More" />
+            <Button variant="outlined" className="sidebar__tweet" fullWidth>
+                Tweet
+            </Button>
+        </div>
+    );
+}
+
+export default Sidebar;
